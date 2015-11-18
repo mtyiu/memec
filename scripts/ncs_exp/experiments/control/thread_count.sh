a1825f6e1b8dd272a1eb1bfd0ff450189d08982d
Man Tung Yiu <michaelsb2004@gmail.com>
2015-11-18 23:10:14 +0800

    Fix bugs in scripts

--- scripts/ncs_exp/experiments/control/thread_count.sh
+++ scripts/ncs_exp/experiments/control/thread_count.sh
@@ -5,6 +5,10 @@ PLIO_PATH=${BASE_PATH}/plio
 
 threads='2 4 6 8 10 12 16 24 32 48 64 96 128 192 256 400 512 1000'
 
+for n in 3 4 8 9; do
+	ssh testbed-node$n "screen -S ycsb -p 0 -X stuff \"${BASE_PATH}/scripts/ncs_exp/experiments/master/thread_count.sh $1$(printf '\r')\"" &
+done
+
 for t in $threads; do
 	echo "Running experiment with thread count = $t..."
 	screen -S manage -p 0 -X stuff "${BASE_PATH}/scripts/util/start.sh $1$(printf '\r')"
--- scripts/ncs_exp/experiments/control/workloads.sh
+++ scripts/ncs_exp/experiments/control/workloads.sh
@@ -8,6 +8,10 @@ coding='raid0 raid1 raid5 rdp cauchy rs evenodd'
 threads=64 # '16 32 64 128 256 512 1000'
 workloads='workloada workloadb workloadc workloadf workloadd'
 
+for n in 3 4 8 9; do
+	ssh testbed-node$n "screen -S ycsb -p 0 -X stuff \"${BASE_PATH}/scripts/ncs_exp/experiments/master/workloads.sh $1$(printf '\r')\"" &
+done
+
 for c in $coding; do
 	echo "Preparing for the experiments with coding scheme = $c..."
 
