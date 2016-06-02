b743de52a55ec4b1f1a1db6ea35c605ae8abb215
Helen Chan <hwchan@cse.cuhk.edu.hk>
2016-06-02 17:00:25 +0800

    Add a missing file

---
+++ common/state_transit/state_transit_state.cc
@@ -0,0 +1,40 @@
+#include "state_transit_state.hh"
+
+bool printServerState( RemapState state, char *mod, char *func, char *ip, short port ) {
+	char *stateString = 0;
+	char *unknown = ( char* ) "Unknown";
+	bool isUnknown = false;
+	switch( state ) {
+		case STATE_NORMAL:
+			stateString = ( char* ) "STATE_NORMAL";
+			break;
+		case STATE_INTERMEDIATE:
+			stateString = ( char* ) "STATE_INTERMEDIATE";
+			break;
+		case STATE_COORDINATED:
+			stateString = ( char* ) "STATE_COORDINATED";
+			break;
+		case STATE_DEGRADED:
+			stateString = ( char* ) "STATE_DEGRADED";
+			break;
+		case STATE_WAIT_DEGRADED:
+			stateString = ( char* ) "STATE_WAIT_DEGRADED";
+			break;
+		case STATE_WAIT_NORMAL:
+			stateString = ( char* ) "STATE_WAIT_NORMAL";
+			break;
+		case STATE_UNDEFINED:
+			stateString = ( char* ) "STATE_UNDEFINED";
+			break;
+		default:	
+			stateString = ( char* ) "STATE_UNKNOWN";
+			isUnknown = true;
+			break;
+	}
+	if ( mod == 0 ) mod = unknown;
+	if ( func == 0 ) func = unknown;
+	if ( ip == 0 ) ip = unknown;
+	__INFO__( BLUE, mod, func, "%s %s:%hu", stateString, ip, port );
+	return isUnknown;
+}
+
